a8a9e67973ea1b0c3f8cbf8c0685684d52ed0091
unknown <rchaouchi1@myges.fr>
2025-12-04 05:07:46 +0100

    Ajout des filtres de connexion par région

--- Menu.py
+++ Menu.py
@@ -72,21 +72,25 @@ def main_menu():
             nom = input("Nom : ")
             prenom = input("Prénom : ")
 
-            print("Choix parmi les régions suivantes :")
-            print("1 - Marseille")
-            print("2 - Rennes")
-            print("3 - Grenoble")
-            choix_region = input("Choisissez la région ( 1, 2 ou 3)")
-
-            if choix_region == "1":
-                region = "Marseille"
-            elif choix_region == "2":
-                region = "Rennes"
-            elif choix_region == "3":
-                region = "Grenoble"
+            if utilisateur_connecte['niveau_droits'] == "1":
+                region = utilisateur_connecte['region']
+                print(f"Région : {region} ( votre région )")
             else:
-                print("Region invalide ! Region par défaut : Marseille.")
-                region = "Marseille"
+                print("Choix parmi les régions suivantes :")
+                print("1 - Marseille")
+                print("2 - Rennes")
+                print("3 - Grenoble")
+                choix_region = input("Choisissez la région ( 1, 2 ou 3)")
+
+                if choix_region == "1":
+                    region = "Marseille"
+                elif choix_region == "2":
+                    region = "Rennes"
+                elif choix_region == "3":
+                    region = "Grenoble"
+                else:
+                    print("Region invalide ! Region par défaut : Marseille.")
+                    region = "Marseille"
 
             new_user = User(nom, prenom, region)
             save_user(new_user, "user")
@@ -146,14 +150,24 @@ def main_menu():
 
             utilisateurs = charger_users()
 
-            if len(utilisateurs) == 0:
+            if utilisateur_connecte['niveau_droits'] == "1":
+                utilisateurs_filtres = []
+                for user_filtre in utilisateurs:
+                    if user_filtre['region'] == utilisateur_connecte['region']:
+                        utilisateurs_filtres.append(user_filtre)
+                        print(f"Région : {utilisateur_connecte['region']}")
+            else:
+                utilisateurs_filtres = utilisateurs
+                print("Toutes les régions pour vous Monsieur/Madame le superadmin BIGBOSS")
+
+            if len(utilisateurs_filtres) == 0:
                 print("Aucun utilisateur enregistré")
             else:
-                print(f"Nombre total : {len(utilisateurs)} utilisateurs dans le fichier.")
+                print(f"Nombre total : {len(utilisateurs_filtres)} utilisateurs dans le fichier.")
                 print(f"{'type':<12} / {'nom':<15} / {'prenom':<15} / {'login':<20} / {'niveau':<8} / {'region':<10}") # Permet juste d'aligner correctement vers la gauche de x caractères
                 print("--------------------------------------------------------------------------------------------------------------------")
 
-                for user in utilisateurs:
+                for user in utilisateurs_filtres:
                     print(f"{user['type']:<12} / {user['nom']:<15} / {user['prenom']:<15} / {user['login']:<20} / {user['niveau_droits']:<8} / {user['region']:<10}")
 
             input("Appuyez sur Entrée pour continuer......")
